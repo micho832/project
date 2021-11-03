30c433db849c032f297d47e05745612c4e2d222a
micho832 <michoessam16@gmail.com>
2021-11-03 21:52:43 +0200

    first commit

---
+++ assignment.php
@@ -0,0 +1,22 @@
+<?php 
+//uppercase//
+$str ="hello world";
+$str= strtoupper($str);
+echo $str;
+echo"<br>";
+//lowercase//
+$str= strtolower($str);
+echo $str;
+echo"<br>";
+//fist letter//
+$str1="hello world";
+$str1=ucfirst($str1);
+echo $str1;
+echo "<br>";
+//first letter in the words of the string//
+$str1=ucwords($str1);
+echo $str1;
+echo "<br>";
+$str2='082307';
+
+?>
---
+++ index.php
@@ -0,0 +1,56 @@
+<?php
+
+$age=20;
+$name="abc";
+
+define("test","welcome");
+
+echo 10;
+echo "<br>";
+echo "ali";
+echo"<br>";
+echo 10,20,30;
+echo"<br>";
+echo' [10,20,30]';
+
+$example="<p> welcome to php</p>";
+echo $example;
+$example2=  "            <b>welcome to php</b>       ";
+//$example3=str_repeat($example2,5);
+
+
+
+
+
+
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php</title>
+</head>
+<body>
+    <h1>hello</h1>
+<?php echo $example?>
+<?php echo $example2?>
+</body>
+</html>
